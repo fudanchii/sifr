d5c1c4058f819372fb31c6ef15afe4ad32113c95
Nurahmadie <nurahmadie@gmail.com>
2013-08-16 08:49:14 +0700

    Expand irc stub, still unusable

--- irc/irc.go
+++ irc/irc.go
@@ -9,12 +9,27 @@ type Client struct {
     conn    net.Conn
 }
 
+type User struct {
+    nick        string
+    username    string
+    host        string
+    realname    string
+    // FIXME: Unencrypted!!!!
+    password    string
+}
+
 func Connect(addr string, port uint32, user User) (*Client, error) {
-    conn, err := net.Dial(addr, port)
-    client = &Client{
-        srvAddr: addr
+    cConn, err := net.Dial("tcp", addr)
+    if err != nil {
+        return nil, 
     }
-    go handleInput(*client.conn)
+    client = &Client{
+        conn: cConn
+    }
+    err = client.register(user)
+    if err != nil {
+    }
+    go client.handleInput()
     return client, nil
 }
 
@@ -23,6 +38,10 @@ func (c *Client) Send(cmd string) {
     c.writer.Flush()
 }
 
-func handleInput(conn *net.Conn) () {
+func (c *Client) handleInput() {
+    reader := bufio.NewReader(c.conn)
+    defer c.conn.Close()
+    for {
 
+    }
 }
